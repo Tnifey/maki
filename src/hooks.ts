2da236f4b1f0e6269a69c520d6cbf5001bb720dd
Tnifey <tnifey@gmail.com>
2024-03-30 18:54:22 +0100

    use with guard

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,13 +2,21 @@ import { type Atom, atomSubscribe, getAtomValue, setAtomValue, toAtom } from "./
 import { getCurrentContext } from "./runtime";
 
 export type Use<T> = ReturnType<typeof use<T>>;
-export function use<T>(initialValue: T | Atom<T>) {
+
+export type Guard<T> = (x: T) => T;
+
+export function use<T>(initialValue: T | Atom<T>, guard: Guard<T> = x => x) {
+    if (typeof guard !== "function") throw new Error("Guard must be a function");
+
     const context = getCurrentContext();
     if (!context) throw new Error("Cannot call use() outside of a component");
     const atomic = toAtom(initialValue);
     const unsub = atomSubscribe(atomic, () => context.render());
     const getter = () => getAtomValue(atomic);
-    const setter = (fn: T | ((x: T) => T)) => setAtomValue(atomic, fn);
+    const setter = (fn: T | ((x: T) => T)) => {
+        const value = guard(isFunction(fn) ? fn(getAtomValue(atomic)) : fn);
+        return setAtomValue(atomic, value);
+    };
 
     /**
      * Get atom value
@@ -25,4 +33,9 @@ export function use<T>(initialValue: T | Atom<T>) {
     }
 
     return Object.assign(getset, atomic, { unsub }, [getter, setter, atomic] as const);
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function isFunction(value: any): value is (...args: any[]) => any {
+    return typeof value === "function";
 }
