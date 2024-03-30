94b349bf1ef1e5172e342a5bd103a0ce961eb4e3
Hubert <tnifey@gmail.com>
2024-03-30 19:02:04 +0100

    use with guard (#2)
    * use with guard

--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,27 @@
-import { component, html } from "./main";
+import { component, html, use } from "./main";
 
 component(() => {
     return () => html`
-        Hello, World!
+        <div class="p-8">
+            <app-counter></app-counter>
+        </div>
     `;
 }).as('app-root');
+
+component(() => {
+    const value = use(0, (x) => Math.max(0, Math.min(10, x)));
+
+    function inc() {
+        value((value) => value + 1);
+    }
+
+    function dec() {
+        value((value) => value - 1);
+    }
+
+    return () => html`
+        <button @click=${dec} type="button" class="px-4 py-2">dec</button>
+        <button @click=${inc} type="button" class="px-4 py-2">inc</button>
+        <code class="px-4 py-2">${value()}</code>
+    `;
+}).as('app-counter');
