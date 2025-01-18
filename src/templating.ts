3bb9023ba29ba99ff77e6fee644596c1845fe464
Tnifey <tnifey@gmail.com>
2025-01-18 16:20:42 +0100

    feat: import type

--- src/templating.ts
+++ src/templating.ts
@@ -17,8 +17,8 @@ export type Renderable = RenderableValue | RenderableValue[];
 
 export { html, svg } from "lit-html";
 
-export { type Configuration } from "twind";
-export { type TwindObserver } from "twind/observe";
+export type { Configuration } from "twind";
+export type { TwindObserver } from "twind/observe";
 
 export const sheet = cssomSheet({ target: new CSSStyleSheet() });
 export const twind = create({ sheet, mode: "silent" });
