ec32c078a6ae9d570ecff536d8889892afaa9a7d
Tnifey <tnifey@gmail.com>
2025-10-25 21:50:35 +0200

    feat: utils function for retrieve composed event target

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ export * from "./state";
 export * from "./templating";
 export * from "./component";
 export * from "./hooks";
+export * from "./utils";
 
 export const VERSION = "0.0.1";
---
+++ src/utils/getEventTarget.ts
@@ -0,0 +1,21 @@
+/**
+ * Get the event target, considering composed events. A helper function to retrieve
+ * the event target in cases where Shadow DOM is involved.
+ * If the event is composed, return the first element in the composed path.
+ * Otherwise, return the event target.
+ * @param event - Event object
+ * @returns Event target or first element in the composed path
+ *
+ * Same as event.composedPath()[0] if composed
+ * Otherwise same as event.target
+ * 
+ * @example
+ * element.addEventListener("click", (event) => {
+ *    const target = getEventTarget(event);
+ *    console.log(target);
+ * });
+ */
+export function getEventTarget(event: Event): EventTarget | null {
+    if (event?.composed) return event.composedPath()?.[0];
+    return event?.target;
+}
